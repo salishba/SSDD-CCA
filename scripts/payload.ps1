dfe162041c3002608e7fee27f3ff556568a813fc
salishba <salishbaliaquat@gmail.com>
2026-05-09 23:00:57 +0500

    entire code

--- scripts/payload.ps1
+++ scripts/payload.ps1
@@ -1,7 +1,7 @@
 $headers = @{
-    "x-access-token" = "=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3NzgxNjcyNDB9.QC36LfDB6vz7Bodhb8QQDHq6cNMsqjzKOLPzXyhMgRM"
+    "x-access-token" = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3NzgzNDQ3ODR9.Taszmkr7BUFvonYL46GgCaPW09O9-IEOLnzsMJcSUdE"
 }
 
 Invoke-WebRequest `
-    -Uri "http://127.0.0.1:5000/api/post/3" `
+    -Uri "http://127.0.0.1:5000/api/post/1" `
     -Headers $headers
